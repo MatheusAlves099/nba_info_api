538c63d6505a6df7c8648ce200e3b253d662f314
MatheusAlves099 <mts02501@gmail.com>
2023-05-04 11:03:08 -0300

    update

---
+++ js/router.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const routes = {
+    '/'         : '/pages/home.html',
+    '/teams' : '/pages/teams.html',
+    '/players'     : '/pages/players.html',
+}
+
+const route = async () => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+
+    const response = await fetch(routes[path])
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+}
+
+window.route = route
